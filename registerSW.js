60897aed9f23226f2e4f4e69d52038925d23a96e
gsnpr <86694106+gsnpr@users.noreply.github.com>
2026-06-04 17:23:18 -0400

    app updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/KittyBudget/sw.js', { scope: '/KittyBudget/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
